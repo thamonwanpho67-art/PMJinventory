650df7589814f807ef19033933406697c5a90295
thamonwanpho67-art <thamonwan.pho67@psru.ac.th>
2025-11-12 14:17:34 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'public.blob.vercel-storage.com',
+        port: '',
+        pathname: '**',
+      },
     ],
     unoptimized: process.env.NODE_ENV === 'development',
   },
